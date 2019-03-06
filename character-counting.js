13e34f687a81612f73f9e7131cdbdf751a752d93
karen1au <kagemikaka@hotmail.com>
2019-03-06 17:38:53 +0000

    after debugged

---
+++ character-counting.js
@@ -0,0 +1,16 @@
+function countletters(string){
+ var string = string.split(' ').join('')
+ string = string.split('')
+ var result = { };
+ let firstcounter = 1
+ for (i = 0; i < string.length; i ++ ) {
+   if (! (string[i] in result) ) {
+     result[string[i]] = 1
+   } else {
+     result[string[i]] += 1
+   }
+ }
+ return (result)
+}
+
+console.log(countletters('lighthouse in the house'))
